d164186ebc8fdff7b1e435896d5be314a5e52fe5
Cesar Jaldin <cesar.jaldin@trueforce.com>
2016-10-07 17:59:56 -0400

    Fourth Refactoring

--- Java/com/gildedrose/GildedRose.java
+++ Java/com/gildedrose/GildedRose.java
@@ -11,7 +11,7 @@ class GildedRose {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                applyPositiveQuality(0, "Sulfuras, Hand of Ragnaros", i);
+                applyNegativeQuality(0, "Sulfuras, Hand of Ragnaros", i);
             } else {
                 if (filterQuality(i, 50)) {
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
@@ -28,7 +28,7 @@ class GildedRose {
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        applyPositiveQuality(0, "Sulfuras, Hand of Ragnaros", i);
+                        applyNegativeQuality(0, "Sulfuras, Hand of Ragnaros", i);
                     } else {
                         resetQuality(i);
                     }
@@ -58,7 +58,7 @@ class GildedRose {
         items[i].quality = items[i].quality - items[i].quality;
     }
 
-    private void applyQuality(String name, int i)
+    private void applyDecrementQuality(String name, int i)
     {
         if (!items[i].name.equals(name)) {
             decrementQuality(i);
@@ -83,10 +83,10 @@ class GildedRose {
         }
     }
 
-    private void applyPositiveQuality(int filter, String name, int i)
+    private void applyNegativeQuality(int filter, String name, int i)
     {
         if (items[i].quality > filter) {
-            applyQuality(name, i);
+            applyDecrementQuality(name, i);
         }
     }
 }
